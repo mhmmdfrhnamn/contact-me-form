ff464a4a02c36a81f53ea0fab20ede37bb6910d4
Muhammad Farhan Amien <farhanexcellentz17@gmail.com>
2024-11-11 20:14:01 +0700

    mengkopy semua file dan ditaruh di folder anyar

--- pesan.php
+++ pesan.php
@@ -1,27 +1,27 @@
 <?php
-    //let's get all form values
-    $nama = $_POST['nama'];
-    $email = $_POST['email'];
-    $telepon = $_POST['telepon'];
-    $lokasi = $_POST['lokasi'];
-    $pesan = $_POST['pesan'];
+    // //let's get all form values
+    // $nama = $_POST['nama'];
+    // $email = $_POST['email'];
+    // $telepon = $_POST['telepon'];
+    // $lokasi = $_POST['lokasi'];
+    // $pesan = $_POST['pesan'];
 
-    if(!empty($email) &&  !empty($pesan)){ //if email and message field is not empty
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) { //if user entered email is valid
-            $receiver = "farhanexcellentz17@gmail.com"; //email penerima pesan 
-            $subjek = "From: $nama <$email>"; 
-            $body = "Nama : $nama\nEmail : $email\nTelepon :$telepon\nLokasi : $lokasi\nPesan : $pesan\n\nRegards, \n$nama";
-            $sender = "From : $email"; //sender email
-            if(mail($receiver, $subjek, $body, $sender,)){
-                echo "Pesan Kamu berhasil dikirim";
-            } else {
-                echo "Maaf, Pesanmu gagal dikirim";
-            }
+    // if(!empty($email) &&  !empty($pesan)){ //if email and message field is not empty
+    //     if(filter_var($email, FILTER_VALIDATE_EMAIL)) { //if user entered email is valid
+    //         $receiver = "farhanexcellentz17@gmail.com"; //email penerima pesan 
+    //         $subjek = "From: $nama <$email>"; 
+    //         $body = "Nama : $nama\nEmail : $email\nTelepon :$telepon\nLokasi : $lokasi\nPesan : $pesan\n\nRegards, \n$nama";
+    //         $sender = "From : $email"; //sender email
+    //         if(mail($receiver, $subjek, $body, $sender,)){
+    //             echo "Pesan Kamu berhasil dikirim";
+    //         } else {
+    //             echo "Maaf, Pesanmu gagal dikirim";
+    //         }
 
-        } else {
-            echo "Masukkan email yang valid";
-        }
-    }else {
-        echo "Email dan pesan harus diisii";
-    }
-?>
+    //         } else {
+    //             echo "Masukkan email yang valid";
+    //         }
+    //     }else {
+    //         echo "Email dan pesan harus diisii";
+    //     }
+    ?>
--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const form = document.querySelector("form"),
-statusTxt = form.querySelector(".button-area span");
+// const form = document.querySelector("form"),
+// statusTxt = form.querySelector(".button-area span");
 
-form.onsubmit = (e)=> {
-    e.preventDefault(); //preventing form from submitting
-    statusTxt.style.color = "gray"
-    statusTxt.style.display = "block";
+// form.onsubmit = (e)=> {
+//     e.preventDefault(); //preventing form from submitting
+//     statusTxt.style.color = "gray"
+//     statusTxt.style.display = "block";
 
-    let xhr = new XMLHttpRequest(); //creating new XML object
-    xhr.open("POST", "pesan.php", true); //sending post request to pesan.php file
+//     let xhr = new XMLHttpRequest(); //creating new XML object
+//     xhr.open("POST", "pesan.php", true); //sending post request to pesan.php file
 
-    xhr.onload = ()=> { //once ajax loaded
-        if(xhr.readyState == 4 && xhr.status == 200){ //if ajax response is 200 & ready status is 4 means there is no error
-            let response = xhr.response; //storing ajax response in a response variable
+//     xhr.onload = ()=> { //once ajax loaded
+//         if(xhr.readyState == 4 && xhr.status == 200){ //if ajax response is 200 & ready status is 4 means there is no error
+//             let response = xhr.response; //storing ajax response in a response variable
 
-            //jika respon error seperti salah memasukkan email maka codingan akan mengubah status warna menjadi merah
-            if(response.indexOf("Email dan pesan harus diisi") != -1 || response.indexOf("Masukkan email yang valid") || response.indexOf("Maaf, Pesanmu gagal dikirim")) {
-                statusTxt.style.color = "red"
-            } else {
-                form.reset();
-                setTimeout(()=> {
-                    statusTxt.style.display = "none";
-                },3000)
-            } 
-            statusTxt.innerHTML = response;
-        }
-    }
-    let formData = new FormData(form); //creating new formData obj. this obj is used to send form data
-    xhr.send(formData); //sending form data
+//             //jika respon error seperti salah memasukkan email maka codingan akan mengubah status warna menjadi merah
+//             if(response.indexOf("Email dan pesan harus diisi") != -1 || response.indexOf("Masukkan email yang valid") || response.indexOf("Maaf, Pesanmu gagal dikirim")) {
+//                 statusTxt.style.color = "red"
+//             } else {
+//                 form.reset();
+//                 setTimeout(()=> {
+//                     statusTxt.style.display = "none";
+//                 },3000)
+//             } 
+//             statusTxt.innerHTML = response;
+//         }
+//     }
+//     let formData = new FormData(form); //creating new formData obj. this obj is used to send form data
+//     xhr.send(formData); //sending form data
 
-}
+// }
